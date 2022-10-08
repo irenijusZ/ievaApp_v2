ef15e0fcc07a6f5533fcb857d03860f3d100c238
kashi <kashifrajput992@gmail.com>
2022-10-08 18:03:59 +0500

    tooltip, arrow direction, multiselect dropdwon fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,6 +101,11 @@ dateCard.forEach(r => {
   })
 })
 
+// popover script
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
 
 // rating js
 $('.rating').starRating(
