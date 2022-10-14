8a0087fdac83897c6710b07841756ecf94e1d169
kashi <kashifrajput992@gmail.com>
2022-10-14 15:16:10 +0500

    mood modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,7 @@ dateCard.forEach(r => {
 // popover script
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
+  return new bootstrap.Popover(popoverTriggerEl);
 })
 
 // rating js
